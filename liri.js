b98d97485d837126b34b64cb9cfdc241ef7b518d
JBloodBeardN <jtn.neese@gmail.com>
2019-03-29 23:00:17 -0500

    ace of base fix

--- liri.js
+++ liri.js
@@ -19,11 +19,11 @@ commandObject = {
         artistORBandORSongORMovie = parameter;
 
 
-         queryUrl = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp"
+        queryUrl = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp"
 
-           axios.get(queryUrl).then(function(response){
-        var objectOfVenues = [];
-        for (i=0;i<response.data.length;i++){
+        axios.get(queryUrl).then(function (response) {
+            var objectOfVenues = [];
+            for (i = 0; i < response.data.length; i++) {
                 var venue = response.data[i].venue.name;
                 var city = response.data[i].venue.city;
                 var region = response.data[i].venue.region;
@@ -34,20 +34,20 @@ commandObject = {
                 //      * Venue location; 
                 //      * Date of the Event (use moment to format this as "MM/DD/YYYY")
                 var venueObject = {
-                    name : venue,
-                    location : city+", "+region+" ,"+country,
-                    date : formatDate
+                    name: venue,
+                    location: city + ", " + region + " ," + country,
+                    date: formatDate
                 }
-            
 
-            objectOfVenues.push(venueObject);
-            
-        }
-        console.log(objectOfVenues);
+
+                objectOfVenues.push(venueObject);
+
+            }
+            console.log(objectOfVenues);
         });
-        
+
         //then log that you did something
-        fs.appendFile("log.txt", ";concert-this,"+artistORBandORSongORMovie, function (err) {
+        fs.appendFile("log.txt", ";concert-this," + artistORBandORSongORMovie, function (err) {
 
             if (err) {
                 return console.log(err);
@@ -63,34 +63,52 @@ commandObject = {
         // if(artistORBandORSongORMovie === null){
         // artistORBandORSongORMovie = "Ace of Base"
         // }
-        if(parameter=== undefined){
-            artistORBandORSongORMovie = "The Sign"
+        if (parameter === "") {
+            artistORBandORSongORMovie = "The+Sign"
+            spotify.search({
+                type: 'track',
+                query: '"' + artistORBandORSongORMovie + '"',
+
+            }).then(function (response) {
+                var artists = [];
+                response.tracks.items[4].album.artists.forEach(function (element, index) {
+                    artists.push(response.tracks.items[4].album.artists[index].name)
+                })
+                console.log("track name: " + response.tracks.items[4].name);
+                console.log("artist : " + artists);
+                console.log("album name: " + response.tracks.items[4].album.name);
+                console.log("preview song here: " + response.tracks.items[4].preview_url);
+
+
+
+            }).catch(function (err) {
+                console.log(err);
+            });
         } else {
             artistORBandORSongORMovie = parameter;
+
+
+
+            spotify.search({
+                type: 'track',
+                query: '"' + artistORBandORSongORMovie + '"',
+                limit: 10,
+
+            }).then(function (response) {
+                var artists = [];
+                response.tracks.items[0].album.artists.forEach(function (element, index) {
+                    artists.push(response.tracks.items[0].album.artists[index].name)
+                })
+                console.log("track name: " + response.tracks.items[0].name);
+                console.log("artist : " + artists);
+                console.log("album name: " + response.tracks.items[0].album.name);
+                console.log("preview song here: " + response.tracks.items[0].preview_url);
+
+            }).catch(function (err) {
+                console.log(err);
+            });
+
         }
-
-
-        spotify.search({ 
-            type: 'track', 
-            query: '"'+artistORBandORSongORMovie+'"', 
-            limit: 10,
-
-        }).then(function(response) {
-            var artists = [];
-            
-            response.tracks.items[0].album.artists.forEach(function(element, index){
-                artists.push(response.tracks.items[0].album.artists[index].name)
-            })
-            console.log("track name: "+response.tracks.items[0].name);
-            console.log("artist : "+artists);
-            console.log("album name: "+response.tracks.items[0].album.name);
-            console.log("preview song here: "+response.tracks.items[0].preview_url);
-
-        }).catch(function(err) {
-            console.log(err);
-        });
-        
-
         // console.log(response);
         //     Artist(s);  The song's name; A preview link of the song from Spotify;  The album that the song is from
 
@@ -98,9 +116,9 @@ commandObject = {
 
 
         //then log that you did something
-        fs.appendFile("log.txt", ";spotify-this-song,"+artistORBandORSongORMovie, function(err){
-            if(err){
-            return console.log(err);
+        fs.appendFile("log.txt", ";spotify-this-song," + artistORBandORSongORMovie, function (err) {
+            if (err) {
+                return console.log(err);
             }
         });
     },
@@ -111,45 +129,45 @@ commandObject = {
 
         //this is for adding default of mr. nobody
         var extraNullMessage = "";
-        if (artistORBandORSongORMovie === ""){
+        if (artistORBandORSongORMovie === "") {
             artistORBandORSongORMovie = "Mr. Nobody"
-        extraNullMessage = ["If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>","","It's on Netflix!"];
+            extraNullMessage = ["If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>", "", "It's on Netflix!"];
         }
 
-        var queryUrl = "http://www.omdbapi.com/?&apikey=40aab0d7&t="+artistORBandORSongORMovie;
-        axios.get(queryUrl).then(function(response){
+        var queryUrl = "http://www.omdbapi.com/?&apikey=40aab0d7&t=" + artistORBandORSongORMovie;
+        axios.get(queryUrl).then(function (response) {
             //this is the mr nobody extra message print
-            if(extraNullMessage !== ""){
-                extraNullMessage.forEach(function(element){
+            if (extraNullMessage !== "") {
+                extraNullMessage.forEach(function (element) {
                     console.log(element);
                 });
             }
-            console.log(response.data.Title +": Name");
+            console.log(response.data.Title + ": Name");
             console.log(response.data.Year + ": Year");
             console.log(response.data.Ratings[0].Value + ": IMDB rating");
-            console.log(response.data.Ratings[1].Value+ ":Rotten Tomatoes");
-            console.log(response.data.Country+": country");
-            console.log(response.data.Language+": Language");
-            console.log(response.data.Plot+ ": Plot");
-            console.log(response.data.Actors+": Actors");
-        }).catch(function(err){
-            if(err){
+            console.log(response.data.Ratings[1].Value + ":Rotten Tomatoes");
+            console.log(response.data.Country + ": country");
+            console.log(response.data.Language + ": Language");
+            console.log(response.data.Plot + ": Plot");
+            console.log(response.data.Actors + ": Actors");
+        }).catch(function (err) {
+            if (err) {
                 artistORBandORSongORMovie = null;
                 this["movie-this"];
             }
         });
 
-        
+
 
 
         // error: display not found and here is Mr. Nobody
 
 
         //then log that you did something
-        fs.appendFile("log.txt", ";movie-this,"+artistORBandORSongORMovie, function(err){
+        fs.appendFile("log.txt", ";movie-this," + artistORBandORSongORMovie, function (err) {
 
-            if(err){
-            return console.log(err);
+            if (err) {
+                return console.log(err);
             }
         });
 
@@ -167,7 +185,7 @@ commandObject = {
                 return console.log(err);
             }
             var fileArray = data.split(";");
-            fileArray.forEach(function(element){
+            fileArray.forEach(function (element) {
                 var command = element.split(",")[0];
                 var parameter = element.split(",")[1];
                 commandObject[command](parameter);
